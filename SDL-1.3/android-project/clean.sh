bf389dacf3f975b3e640fdf3b12aab216bdc07af
Michael Chen <omxcodec@gmail.com>
2012-10-20 11:29:26 +0800

    remove SDL-1.3 from this project. i will put the sdl to "external/sdl" path

--- SDL-1.3/android-project/clean.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-#OUT=../../../out/target/product/maguro
-if [ "$OUT" = "" ]; then
-    echo OUT variable not set, please lunch your android env
-    exit 1
-fi
-
-find -name "*SDL**" | xargs rm -rf
