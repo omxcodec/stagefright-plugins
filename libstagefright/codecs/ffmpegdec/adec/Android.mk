de27bee6bf0a0609ba90d8da643f96233d6979d8
Michael Chen <omxcodec@gmail.com>
2012-10-28 17:12:04 +0800

    add support for ffmpeg audio decoder, only aac work now! pls "git pull" my "android_frameworks_base"

---
+++ libstagefright/codecs/ffmpegdec/adec/Android.mk
@@ -0,0 +1,65 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+FFMPEG_SRC_DIR   := $(TOP)/external/ffmpeg
+FFMPEG_BUILD_DIR := $(PRODUCT_OUT)/obj/ffmpeg
+
+LOCAL_SRC_FILES := \
+        SoftFFmpegAudio.cpp
+
+LOCAL_C_INCLUDES := \
+        frameworks/base/media/libstagefright/include \
+        frameworks/base/include/media/stagefright/openmax
+
+LOCAL_C_INCLUDES += \
+	$(TOP)/external/sdl/include \
+	$(TOP)/external/stagefright-plugins \
+	$(TOP)/external/stagefright-plugins/libstagefright \
+
+LOCAL_C_INCLUDES += \
+	$(FFMPEG_SRC_DIR) \
+	$(FFMPEG_BUILD_DIR)
+
+LOCAL_SHARED_LIBRARIES := \
+        libutils          \
+        libcutils         \
+        libstagefright    \
+	libstagefright_omx \
+        libstagefright_foundation
+
+LOCAL_SHARED_LIBRARIES +=  \
+        libffmpeg_utils    \
+        libSDL
+
+FFMPEG_BUILD_LIBS := \
+	-L$(FFMPEG_BUILD_DIR)/libavutil         \
+        -L$(FFMPEG_BUILD_DIR)/libavcodec        \
+        -L$(FFMPEG_BUILD_DIR)/libswscale        \
+        -L$(FFMPEG_BUILD_DIR)/libpostproc       \
+        -L$(FFMPEG_BUILD_DIR)/libavformat       \
+        -L$(FFMPEG_BUILD_DIR)/libavfilter       \
+        -L$(FFMPEG_BUILD_DIR)/libswresample
+
+LOCAL_LDFLAGS += $(FFMPEG_BUILD_LIBS) \
+        -lavutil       \
+        -lavcodec      \
+        -lpostproc     \
+        -lavformat     \
+        -lavfilter     \
+        -lswresample   \
+        -lswscale
+
+LOCAL_MODULE := libstagefright_soft_ffmpegadec
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(TARGET_DEVICE),maguro)
+    LOCAL_CFLAGS += -DUSES_NAM
+endif
+
+ifeq ($(TARGET_ARCH),arm)
+    LOCAL_CFLAGS += -Wno-psabi
+endif
+
+LOCAL_CFLAGS += -D__STDC_CONSTANT_MACROS=1
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ libstagefright/codecs/ffmpegdec/adec/SoftFFmpegAudio.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2012 Michael Chen <omxcodec@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SOFT_FFMPEGAUDIO_H_
+
+#define SOFT_FFMPEGAUDIO_H_
+
+#include "SimpleSoftOMXComponent.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <inttypes.h>
+#include <math.h>
+#include <signal.h>
+#include <limits.h> /* INT_MAX */
+
+#include "config.h"
+#include "libavutil/avstring.h"
+#include "libavutil/colorspace.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/dict.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/avassert.h"
+#include "libavutil/intreadwrite.h"
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+#include "libswscale/swscale.h"
+#include "libavcodec/audioconvert.h"
+#include "libavutil/opt.h"
+#include "libavutil/internal.h"
+#include "libavcodec/avfft.h"
+#include "libswresample/swresample.h"
+
+#include "cmdutils.h"
+
+#include <SDL.h>
+#include <SDL_thread.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+namespace android {
+
+struct SoftFFmpegAudio : public SimpleSoftOMXComponent {
+    SoftFFmpegAudio(const char *name,
+            const OMX_CALLBACKTYPE *callbacks,
+            OMX_PTR appData,
+            OMX_COMPONENTTYPE **component);
+
+protected:
+    virtual ~SoftFFmpegAudio();
+
+    virtual OMX_ERRORTYPE internalGetParameter(
+            OMX_INDEXTYPE index, OMX_PTR params);
+
+    virtual OMX_ERRORTYPE internalSetParameter(
+            OMX_INDEXTYPE index, const OMX_PTR params);
+
+    virtual void onQueueFilled(OMX_U32 portIndex);
+    virtual void onPortFlushCompleted(OMX_U32 portIndex);
+    virtual void onPortEnableCompleted(OMX_U32 portIndex, bool enabled);
+
+private:
+    enum {
+        kNumBuffers = 4,
+        kOutputBufferSize = 4608 * 2
+    };
+
+    enum {
+        MODE_MPEG,
+	MODE_MPEGL1,
+	MODE_MPEGL2,
+        MODE_AAC,
+        MODE_AC3,
+    } mMode;
+
+    enum {
+        kPortIndexInput  = 0,
+        kPortIndexOutput = 1,
+    };
+
+    AVCodecContext *mCtx;
+    struct SwrContext *mSwrCtx;
+
+    bool mExtradataReady;
+    bool mIgnoreExtradata;
+    bool mFlushComplete;
+    bool mSignalledError;
+    bool mReceivedEOS;
+
+    AVFrame *mFrame;
+
+    int64_t mAnchorTimeUs;
+    int64_t mNumFramesOutput;
+    int32_t mInputBufferSize;
+
+    uint8_t mSilenceBuffer[kOutputBufferSize];
+    uint8_t *mPAudioBuffer;
+    int32_t mAudioBufferSize;
+
+
+    int32_t mNumChannels;
+    int32_t mSamplingRate;
+
+    enum AVSampleFormat mAudioSrcFmt;
+    enum AVSampleFormat mAudioTgtFmt;
+    int mAudioSrcChannels;
+    int mAudioTgtChannels;
+    int64_t mAudioSrcChannelLayout;
+    int64_t mAudioTgtChannelLayout;
+    int mAudioSrcFreq;
+    int mAudioTgtFreq;
+
+    enum {
+        NONE,
+        AWAITING_DISABLED,
+        AWAITING_ENABLED
+    } mOutputPortSettingsChange;
+
+    status_t initFFmpeg();
+    void deInitFFmpeg();
+    void setAVCtxToDefault(AVCodecContext *avctx, const AVCodec *codec);
+
+    void initPorts();
+    status_t initDecoder();
+    void deInitDecoder();
+
+    DISALLOW_EVIL_CONSTRUCTORS(SoftFFmpegAudio);
+};
+
+}  // namespace android
+
+#endif  // SOFT_FFMPEGAUDIO_H_
+
+
